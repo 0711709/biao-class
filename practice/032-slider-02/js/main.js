5db0c23bd07ff8cb035c09f1abd931d7e06b9394
1803 <0711709@gmail.com>
2018-11-16 10:12:29 +0800

    11/16 添加注释

--- practice/032-slider-02/js/main.js
+++ practice/032-slider-02/js/main.js
@@ -3,22 +3,26 @@ let current = item.length;
 
 boot();
 
+
+//启动函数
 function boot() {
     slider();
 };
 
+// 轮播函数
 function slider() {
    setInterval(() => {
        current--;
        if (current == 0) {
            current = item.length;
-           reset();
+           reset();//重置
        } else {
            item[current].style.opacity = "0";
        }
    },1000)
 }
 
+//重置函数
 function reset() {
     for (let i = 0; i < item.length; i++){
         item[i].style.opacity = "1";

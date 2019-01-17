65e4aa7b0377f65fe711c4cbebe33333720730cb
1803 <0711709@gmail.com>
2019-01-17 16:45:54 +0800

    1/17 开始 biao circle vue 项目

---
+++ practice/059-biao-circle/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <div class="content">
+        <div class="left">
+          <a href>
+            <img src="../public/logo-sm.png" alt="logo">
+          </a>
+        </div>
+        <div class="right">
+          <a href>登陆</a>
+          <a href>注册</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  components: {}
+};
+</script>
+
+<style>
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>

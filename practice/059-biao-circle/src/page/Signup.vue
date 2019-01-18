8f2a3b24384b5484c95645685e781ab43d05f9f3
1803 <0711709@gmail.com>
2019-01-19 01:19:40 +0800

    1/19 添加密码一致性验证

--- practice/059-biao-circle/src/page/Signup.vue
+++ practice/059-biao-circle/src/page/Signup.vue
@@ -19,6 +19,13 @@
                 <span v-if="error.password" class="error">{{error.password}}</span>
               </label>
             </div>
+            <div class="input-control">
+              <label>
+                <span>重复密码</span>
+                <input type="password" v-model="current.password2">
+                <span v-if="error.password2" class="error">{{error.password2}}</span>
+              </label>
+            </div>
             <button type="submit">注册</button>
           </form>
         </div>
@@ -42,7 +49,8 @@ export default {
       current: {},
       error: {
         username: null,
-        password: null
+        password: null,
+        password2: null
       }
     };
   },
@@ -68,17 +76,23 @@ export default {
       let e = this.error;
       let username = this.current.username;
       let password = this.current.password;
+      let password2 = this.current.password2;
 
-      let validateUsername = !username || !/[a-zA-Z0-9]{4,20}/.test(username);
-      let validatePassword = !password || password.length < 6;
+      let inValidateUsername = !username || !/[a-zA-Z0-9]{4,20}/.test(username);
+      let inValidatePassword = !password || password.length < 6;
+      let inValidatePassword2 = !password2 || password2 !== password;
 
-      validateUsername
+      inValidateUsername
         ? (e.username = "用户名格式有误,长度应在4到20位之间")
         : (e.username = null);
 
-      validatePassword
+      inValidatePassword
         ? (e.password = "密码长度应大于6位")
         : (e.password = null);
+
+      inValidatePassword2
+        ? (e.password2 = "两次密码输入不一致")
+        : (e.password2 = null);
 
       return !e.username && !e.password;
     }

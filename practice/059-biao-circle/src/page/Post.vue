dfe62f7cd76acec4098d6a733ef3abbbc353f0be
1803 <0711709@gmail.com>
2019-03-19 18:46:46 +0800

    3/19 完成 post 详情页样式

--- practice/059-biao-circle/src/page/Post.vue
+++ practice/059-biao-circle/src/page/Post.vue
@@ -1,26 +1,50 @@
 <template>
-    <div id="main">
-      <div class="container">
-        <div class="section">
-          <div>
-            <div class="box">表圈子</div>
-            <div class="box title">title</div>
-            <div class="box content">content</div>
-            <div class="box comment">comment</div>
+  <div id="main">
+    <div class="container">
+      <div id="post" class="section">
+        <div>
+          <div class="box">
+            <div class="title">title</div>
+            <div class="info">
+              <span>name</span>
+              <span>发布于</span>
+              <span>time</span>
+            </div>
+          </div>
+          <div class="box content">content</div>
+          <div class="comment-area">
+            <div class="box comment">
+              <div class="head">
+                <span>name</span>
+                <span>发布于</span>
+                <span>time</span>
+              </div>
+              <div class="content">content</div>
+            </div>
+            <div class="box comment">1</div>
+            <div class="box comment">1</div>
+          </div>
+          <div class="box sub-comment">
+            <form>
+              <div class="head">添加回复</div>
+              <textarea placeholder="请尽量让自己的回复可以帮助其他人"></textarea>
+              <button>回复</button>
+            </form>
           </div>
         </div>
-        <div class="side">
-          <div class="box">
-            <div class="me" v-if="session.user()">
-              <router-link to="/member" class="post">{{session.user().username}}</router-link>
-              <div class="collection">收藏</div>
-              <router-link to="/member" class="post">创建新主题</router-link>
-              <div class="message">消息</div>
-            </div>
+      </div>
+      <div class="side">
+        <div class="box">
+          <div class="me" v-if="session.user()">
+            <router-link to="/member" class="post">{{session.user().username}}</router-link>
+            <div class="collection">收藏</div>
+            <router-link to="/member" class="post">创建新主题</router-link>
+            <div class="message">消息</div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -41,17 +65,13 @@ export default {
 
   methods: {
     read() {
-      api("post/find", { with: [{ model: "user", relation: "belongs_to" }] }).then(r => {
+      api("post/find", {
+        id: this.$route.params.id,
+        with: [{ model: "user", relation: "belongs_to" }]
+      }).then(r => {
         this.list = r.data;
+        console.log(this.list);
       });
-    },
-
-    postDelete(id) {
-      api("post/delete", {id}).then(r => {
-        if(r.success) {
-          this.read();
-        }
-      })
     }
   }
 };

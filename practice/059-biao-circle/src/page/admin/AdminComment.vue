c90e0cee1aa488d1ee7ad9e8e18bf449174f5e88
1803 <0711709@gmail.com>
2019-03-21 19:24:47 +0800

    3/21 完成帖子和评论管理的页面

--- practice/059-biao-circle/src/page/admin/AdminComment.vue
+++ practice/059-biao-circle/src/page/admin/AdminComment.vue
@@ -1,3 +1,66 @@
 <template>
-    <div>comment</div>
+  <div id="admin">
+    <div class="header">
+      <div class="title">评论管理</div>
+    </div>
+    <div class="list">
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>作者</th>
+          <th>日期</th>
+          <th>内容</th>
+          <th>操作</th>
+        </thead>
+        <tbody>
+          <tr v-for="(it, index) in list" :key="index">
+            <td>{{it.id}}</td>
+            <td>{{it.$user && it.$user.username || "-"}}</td>
+            <td>{{it.create_at}}</td>
+            <td>{{it.content}}</td>
+            <td>
+              <button @click="deleteUser(it.id)">删除</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+
+<script>
+import store from "../../lib/store";
+import api from "../../lib/api";
+
+export default {
+  data() {
+    return {
+      list: {},
+    };
+  },
+
+  mounted() {
+    this.read();
+  },
+
+  methods: {
+    read() {
+      //, { except: ["password"] }
+      api("comment/read", {with: {model: "user", relation: "belongs_to"}}).then(r => {
+        this.list = r.data;
+      });
+    },
+
+    deleteUser(id) {
+      api("comment/delete", { id }).then(r => {
+        this.read();
+      });
+    },
+  }
+};
+</script>
+
+<style>
+</style>
+
+
--- practice/059-biao-circle/src/page/admin/AdminPost.vue
+++ practice/059-biao-circle/src/page/admin/AdminPost.vue
@@ -1,3 +1,65 @@
 <template>
-    <div>post</div>
+  <div id="admin">
+    <div class="header">
+      <div class="title">帖子管理</div>
+    </div>
+    <div class="list">
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>标题</th>
+          <th>日期</th>
+          <th>作者</th>
+          <th>操作</th>
+        </thead>
+        <tbody>
+          <tr v-for="(it, index) in list" :key="index">
+            <td>{{it.id}}</td>
+            <td>{{it.title}}</td>
+            <td>{{it.create_at}}</td>
+            <td>{{it.$user && it.$user.username || "-"}}</td>
+            <td>
+              <button @click="deleteUser(it.id)">删除</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+
+<script>
+import store from "../../lib/store";
+import api from "../../lib/api";
+
+export default {
+  data() {
+    return {
+      list: {},
+    };
+  },
+
+  mounted() {
+    this.read();
+  },
+
+  methods: {
+    read() {
+      //, { except: ["password"] }
+      api("post/read", {with: {model: "user", relation: "belongs_to"}}).then(r => {
+        this.list = r.data;
+      });
+    },
+
+    deleteUser(id) {
+      api("post/delete", { id }).then(r => {
+        this.read();
+      });
+    },
+  }
+};
+</script>
+
+<style>
+</style>
+

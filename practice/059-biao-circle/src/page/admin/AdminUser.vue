450e7b56d8297e4da65ce0a77eafb57886428801
1803 <0711709@gmail.com>
2019-03-27 19:56:35 +0800

    3/27 完成用户名的验证测试

--- practice/059-biao-circle/src/page/admin/AdminUser.vue
+++ practice/059-biao-circle/src/page/admin/AdminUser.vue
@@ -10,9 +10,15 @@
       </button>
       <div v-if="createOrCancel">
         <form @submit.prevent="createOrUpdate">
-          <label>
+          <label @keyup="validate(current.username, 'username')">
             <div>用户名</div>
             <input type="text" v-model="current.username">
+            <div
+              class="error"
+              v-for="(value, e) in errors.username"
+              :key="e"
+              v-if="value"
+            >{{rules.username[e].msg}}</div>
           </label>
           <label>
             <div>昵称</div>
@@ -55,13 +61,45 @@
 <script>
 import store from "../../lib/store";
 import api from "../../lib/api";
+import valee from "../../lib/valee";
 
 export default {
   data() {
     return {
-      createOrCancel: false,
+      createOrCancel: true,
       list: {},
-      current: {}
+      current: {},
+      errors: {
+        // username: { lengthBetween: true, regex: true }
+      },
+      rules: {
+        username: {
+          // unique: {
+          //   params: ["user/exists"],
+          //   msg: "用户名已存在"
+          // },
+          lengthBetween: {
+            params: [4, 12],
+            msg: "用户名长度应在4至12位之间"
+          },
+          regex: {
+            params: [/^[a-zA-Z]+[0-9]*$/],
+            msg: "用户名应由字母和数字组成"
+          }
+        },
+        nickname: {
+          required: {
+            msg: "此项为必填项"
+          },
+          regex: {
+            params: [/^[a-zA-Z]+[0-9]*$/],
+            msg: "用户名应由字母和数字组成"
+          }
+        }
+      },
+      formStatus: {
+        username: "pending"
+      }
     };
   },
 
@@ -77,8 +115,42 @@ export default {
       });
     },
 
+    validate(value, field) {
+      let ruleObj = this.rules[field];
+
+      for (let key in ruleObj) {
+        let rule = ruleObj[key];
+        let valid = valee[key](this.current[field], ...rule.params);
+        if (!this.errors[field]) {
+          // this.errors[field] = {};
+          this.$set(this.errors, field, {});
+        }
+        this.errors[field][key] = !valid;
+      }
+    },
+
+    // setError(field, type) {
+    //   let errorList = this.error[field];
+    //   if (!errorList) {
+    //     errorList = this.error[field] = [];
+    //   }
+    //   errorList.push(type);
+    // },
+
+    // removeError(field, type) {
+    //   let errorList = this.error[field];
+    //   if (!errorList) {
+    //     return;
+    //   }
+    //   let index = errorList.findIndex(it => it === type);
+    //   if (index === -1) {
+    //     return;
+    //   }
+    //   errorList.splice(index, 1);
+    // },
+
     deleteUser(id) {
-      if(!confirm("确定删除")){
+      if (!confirm("确定删除")) {
         return;
       }
       api("user/delete", { id }).then(r => {
@@ -93,11 +165,11 @@ export default {
       } else {
         action = "create";
       }
-      api(`user/${action}`, this.current).then(r=>{
-          this.read();
-          this.current = {};
-          this.createOrCancel = false;
-      })
+      api(`user/${action}`, this.current).then(r => {
+        this.read();
+        this.current = {};
+        this.createOrCancel = false;
+      });
     }
   }
 };

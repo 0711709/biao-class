0f3088428f39116d7f1d190c026ecab5e9ae1203
1803 <0711709@gmail.com>
2019-04-08 17:16:17 +0800

    4/8 安装 axios  router element-ui

--- project/snack/src/main.js
+++ project/snack/src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import Router from "vue-router";
+
+import Home from "./Home.vue"
+
+
+
+Vue.use(ElementUI);
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    }
+  ]
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')

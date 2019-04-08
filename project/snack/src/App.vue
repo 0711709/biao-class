90c82aeec3124369d5cf273db721b3c33e784b43
1803 <0711709@gmail.com>
2019-04-08 17:29:23 +0800

    4/8 大致确定需要的页面

--- project/snack/src/App.vue
+++ project/snack/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <span>yo</span>
     <router-view></router-view>
   </div>
 </template>
--- project/snack/src/main.js
+++ project/snack/src/main.js
@@ -7,6 +7,12 @@ import 'element-ui/lib/theme-chalk/index.css';
 import Router from "vue-router";
 
 import Home from "./components/Home.vue"
+import Product from "./components/Product.vue"
+import Signup from "./components/Signup.vue"
+import Login from "./components/Login.vue"
+import Search from "./components/Search.vue"
+
+
 
 
 
@@ -18,7 +24,23 @@ const router = new Router({
     {
       path: "/",
       component: Home,
-    }
+    },
+    {
+      path: "/product",
+      component: Product
+    }, 
+    {
+      path: "/signup",
+      component: Signup
+    }, 
+    {
+      path: "/login",
+      component: Login
+    },
+    {
+      path: "/search",
+      component: Search
+    },
   ]
 })
 

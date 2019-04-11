791c2a2dd8a45cca58be07c936ab65504838bc21
1803 <0711709@gmail.com>
2019-04-11 21:54:54 +0800

    4/11 完善注册功能

--- project/snack/src/components/Signup.vue
+++ project/snack/src/components/Signup.vue
@@ -6,29 +6,36 @@
         <el-tabs v-model="activeName">
           <el-tab-pane label="手机注册" name="phone">
             <el-form-item label="手机号">
-              <el-input v-model="form.phone"></el-input>
-              <span v-if="errors.phone">{{errors.phone}}</span>
+              <el-input v-model="form.phone" @keyup.native="debounceValidate"></el-input>
+              <span class="error" v-if="errors.phone">{{errors.phone}}</span>
+              <span class="loading-text" v-if="pending"><i class="el-icon-loading loading"></i>查询中</span>
             </el-form-item>
           </el-tab-pane>
           <el-tab-pane label="邮箱注册" name="mail">
             <el-form-item label="邮箱">
-              <el-input v-model="form.mail"></el-input>
-              <span v-if="errors.mail">{{errors.mail}}</span>
+              <el-input v-model="form.mail" @keyup.native="debounceValidate"></el-input>
+              <i v-if="pending" class="el-icon-loading loading">查询中</i>
+              <span class="error" v-if="errors.mail">{{errors.mail}}</span>
             </el-form-item>
           </el-tab-pane>
         </el-tabs>
         <el-form-item label="验证码">
           <el-input v-model="formCode">
-            <el-button slot="append" class="button" @click="sendCode" :disabled="!!countDown">
+            <el-button
+              slot="append"
+              class="button"
+              @click="sendCode"
+              :disabled="!!countDown || !canSend"
+            >
               <span v-if="countDown">{{countDown}}s后获取</span>
               <span v-else>发送验证码</span>
             </el-button>
           </el-input>
-          <span v-if="errors.code">{{errors.code}}</span>
+          <span class="error" v-if="errors.code">{{errors.code}}</span>
         </el-form-item>
         <el-form-item label="密码">
-          <el-input v-model="form.password" placeholder="密码长度不小于6位"></el-input>
-          <span v-if="errors.password">{{errors.password}}</span>
+          <el-input v-model="form.password" placeholder="密码长度不小于6位" show-password></el-input>
+          <span class="error" v-if="errors.password">{{errors.password}}</span>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" :disabled="!valid" @click="signup">注册</el-button>
@@ -63,7 +70,10 @@ export default {
         code: ""
       },
       code: null,
-      valid: false
+      valid: false,
+      pending: false,
+      timer: null,
+      canSend: false,
     };
   },
 
@@ -72,22 +82,46 @@ export default {
   },
 
   methods: {
+    //跳动验证
+    debounceValidate() {
+      this.canSend = false;
+
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      this.timer = setTimeout(() => {
+        this.validateSignBy();
+      }, 300);
+    },
+
     //验证手机或邮箱
     validateSignBy() {
       let f = this.form;
       let signupBy = this.activeName;
+      
       if (!f[signupBy]) {
         this.errors[signupBy] = `${this.byObj[signupBy]}为必填项`;
-        return false;
+        return;
       }
 
       if (!is[signupBy](f[signupBy])) {
         this.errors[signupBy] = `${this.byObj[signupBy]}格式不合法`;
-        return false;
+        return;
       }
 
+      //查询是否注册
+      this.pending = true;
+      api("user/exists", { where: { and: { [signupBy]: f[signupBy] } } }).then(r => {
+        if(r.success){
+          this.pending = false
+          if(r.data){
+            this.errors[signupBy] = `${this.byObj[signupBy]}已注册`
+          }
+        }
+      })
+
       this.errors[signupBy] = "";
-      return true;
+      this.canSend = true;
     },
 
     //验证密码和验证码
@@ -130,10 +164,6 @@ export default {
       let action;
       let signupBy = this.activeName;
       let param = {};
-
-      if (!this.validateSignBy()) {
-        return;
-      }
 
       this.valid = true;
 
@@ -178,6 +208,16 @@ export default {
 
 .signup .form-container h1 {
   margin-bottom: 3rem;
+}
+
+.signup i.loading {
+  color: #409eff;
+  font-size: 120%;
+}
+
+.signup .loading-text{
+  font-size: 90%;
+  color: #409eff;
 }
 </style>
 

ad407ed15a14937c24fd38ed8547c810fc6385f4
1803 <0711709@gmail.com>
2019-04-19 20:17:16 +0800

    4/19 了解 react 的数据绑定

--- project/todolist/src/Todolist.js
+++ project/todolist/src/Todolist.js
@@ -1,17 +1,41 @@
 import React, { Component, Fragment } from "react";
 
 class Todolist extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: [],
+    }
+  }
+
   render() {
     return (
-      <Fragment><div>
-        <input></input><button>提交</button>
-        <ul>
-          <li>学习 react</li>
-          <li>制作网页</li>
-        </ul>
-      </div></Fragment>
+      <Fragment>
+        <div>
+          <input
+            value={this.state.inputValue}
+            onChange = {this.handleInputChange.bind(this)}
+          />
+          <button>提交</button>
+          <ul>
+            <li>学习 react</li>
+            <li>制作网页</li>
+          </ul>
+        </div>
+      </Fragment>
     )
   }
+
+  handleInputChange(e){
+    // console.log(this)
+    // this.state.inputValue = e.target.value;
+    this.setState({
+      inputValue: e.target.value,
+    })
+  }
+
 }
 
 export default Todolist;

107c72c6de501341be3cad2f67ba216f175d1ad3
1803 <0711709@gmail.com>
2019-04-19 19:29:26 +0800

    4/19 react 初体验 了解 JSX 语法

---
+++ project/todolist/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//等价于
+//import React from "react"
+//const Component = React.Component
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hello world
+      </div>
+    );
+  }
+}
+
+export default App;

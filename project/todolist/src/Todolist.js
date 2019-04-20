e8f2a6b00a77c77577d238f169d6cc5ec2535a39
1803 <0711709@gmail.com>
2019-04-20 17:06:12 +0800

    4/20 了解 react 的数据绑定

--- project/todolist/src/Todolist.js
+++ project/todolist/src/Todolist.js
@@ -6,36 +6,52 @@ class Todolist extends Component {
     super(props);
     this.state = {
       inputValue: "",
-      list: [],
+      list: ["学习 react"],
     }
   }
 
   render() {
     return (
       <Fragment>
-        <div>
-          <input
-            value={this.state.inputValue}
-            onChange = {this.handleInputChange.bind(this)}
-          />
-          <button>提交</button>
-          <ul>
-            <li>学习 react</li>
-            <li>制作网页</li>
-          </ul>
-        </div>
+        <input
+          value={this.state.inputValue}
+          onChange={this.handleInputChange.bind(this)}
+        />
+        <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return <li key={index} onClick={this.handleItemDelete.bind(this, index)}>{item}</li>
+            })
+          }
+        </ul>
       </Fragment>
     )
   }
 
-  handleInputChange(e){
-    // console.log(this)
-    // this.state.inputValue = e.target.value;
+  handleInputChange(e) {
     this.setState({
       inputValue: e.target.value,
     })
   }
 
+  handleBtnClick() {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ""
+    })
+  }
+
+  handleItemDelete(index) {
+    //immutable 
+    // state 不允许我们做任何改变
+    const list = [...this.state.list];
+    list.splice(index, 1);
+
+    this.setState({
+      list: list
+    })
+  }
 }
 
 export default Todolist;

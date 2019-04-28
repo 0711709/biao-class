bbe4904ce7e032b12c121c607e1f080e336c3fec
1803 <0711709@gmail.com>
2019-04-28 18:00:08 +0800

    4/28 用 setTimeout 实现 setInterval

--- practice/062-review/1.js
+++ practice/062-review/1.js
@@ -28,157 +28,157 @@ function interval() {
 // console.log(whh.prototype)
 
 // console.log(new User("whh", 19, "male"))
-let arr = [
-    {
-        id: 1,
-        n: 'a',
-    },
-    {
-        id: 2,
-        n: 'b',
-    },
-    {
-        id: 3,
-        n: 'c',
-    },
-    {
-        id: 4,
-        n: 'a1',
-        parent_id: 1
-    },
-    {
-        id: 5,
-        n: 'a2',
-        parent_id: 1
-    },
-    {
-        id: 6,
-        n: 'a3',
-        parent_id: 1
-    },
-    {
-        id: 7,
-        n: 'b1',
-        parent_id: 2
-    },
-    {
-        id: 8,
-        n: 'b2',
-        parent_id: 2
-    },
-    {
-        id: 9,
-        n: 'b3',
-        parent_id: 2
-    },
-    {
-        id: 10,
-        n: 'c1',
-        parent_id: 3
-    },
-    {
-        id: 11,
-        n: 'c2',
-        parent_id: 3
-    },
-    {
-        id: 12,
-        n: 'a1.1',
-        parent_id: 4
-    },
-]
+// let arr = [
+//     {
+//         id: 1,
+//         n: 'a',
+//     },
+//     {
+//         id: 2,
+//         n: 'b',
+//     },
+//     {
+//         id: 3,
+//         n: 'c',
+//     },
+//     {
+//         id: 4,
+//         n: 'a1',
+//         parent_id: 1
+//     },
+//     {
+//         id: 5,
+//         n: 'a2',
+//         parent_id: 1
+//     },
+//     {
+//         id: 6,
+//         n: 'a3',
+//         parent_id: 1
+//     },
+//     {
+//         id: 7,
+//         n: 'b1',
+//         parent_id: 2
+//     },
+//     {
+//         id: 8,
+//         n: 'b2',
+//         parent_id: 2
+//     },
+//     {
+//         id: 9,
+//         n: 'b3',
+//         parent_id: 2
+//     },
+//     {
+//         id: 10,
+//         n: 'c1',
+//         parent_id: 3
+//     },
+//     {
+//         id: 11,
+//         n: 'c2',
+//         parent_id: 3
+//     },
+//     {
+//         id: 12,
+//         n: 'a1.1',
+//         parent_id: 4
+//     },
+// ]
 
-let obj = {
-    children:[]
-}
+// let obj = {
+//     children: []
+// }
 
-let log = [];
+// let log = [];
 
 //分离出第一层结构
-function boot(){
-    arr.forEach((it, index) => {
-        if(!it.parent_id){
-            obj.children.push(it)
-            log.push(index);
-        }
-    })
-}
+// function boot() {
+//     arr.forEach((it, index) => {
+//         if (!it.parent_id) {
+//             obj.children.push(it)
+//             log.push(index);
+//         }
+//     })
+// }
 
-function next(result){
-    result.forEach(it => {
-        it.children = it.children || [];
-        arr.forEach((e, index) => {
-            if(log.includes(index)){
-                return;
-            }
-            if(e.parent_id === it.id){
-                it.children.push(e);
-                log.push(index);
-                next(it.children);
-            }
-        })
-    })
-      
-}
+// function next(result) {
+//     result.forEach(it => {
+//         it.children = it.children || [];
+//         arr.forEach((e, index) => {
+//             if (log.includes(index)) {
+//                 return;
+//             }
+//             if (e.parent_id === it.id) {
+//                 it.children.push(e);
+//                 log.push(index);
+//                 next(it.children);
+//             }
+//         })
+//     })
+
+// }
 
 
 
-/*
-* 分离每一层
-* @param level Array 需要迭代的层级内数组
-* */
-function starter(level) {
-  if (!level)
-    return;
-  /*迭代当前层级中的每一项*/
-  level.forEach(function (parent) {
-    /*初始化下一级的children*/
-    parent.children = parent.children || [];
+// /*
+// * 分离每一层
+// * @param level Array 需要迭代的层级内数组
+// * */
+// function starter(level) {
+//     if (!level)
+//         return;
+//     /*迭代当前层级中的每一项*/
+//     level.forEach(function (parent) {
+//         /*初始化下一级的children*/
+//         parent.children = parent.children || [];
 
-    /*迭代所有数据*/
-    arr.forEach(function (item, i) {
-      /*如果已经分类储存直接跳过，进行下一次迭代*/
-      if (log.indexOf(i) !== -1)
-        return;
-      /*如果当前对象的父级id等于父级（外部迭代）的id*/
-      if (item.parent_id === parent.id) {
-        /*将当前对象推入父级*/
-        parent.children.push(item);
-        /*记录当前对象的索引，说明这个对象已经分类过了*/
-        log.push(i);
-        starter(parent.children);
-      }
-    });
-  })
-}
+//         /*迭代所有数据*/
+//         arr.forEach(function (item, i) {
+//             /*如果已经分类储存直接跳过，进行下一次迭代*/
+//             if (log.indexOf(i) !== -1)
+//                 return;
+//             /*如果当前对象的父级id等于父级（外部迭代）的id*/
+//             if (item.parent_id === parent.id) {
+//                 /*将当前对象推入父级*/
+//                 parent.children.push(item);
+//                 /*记录当前对象的索引，说明这个对象已经分类过了*/
+//                 log.push(i);
+//                 starter(parent.children);
+//             }
+//         });
+//     })
+// }
 /*先找到并分类顶级对象*/
 
 
-boot();
-next(obj.children);
+// boot();
+// next(obj.children);
 // starter(obj.children);
 // console.log(obj, log)
 
 
 
-let whh = {
-    name: "王花花",
-    age: 18,
-    gender: "male",
-}
+// let whh = {
+//     name: "王花花",
+//     age: 18,
+//     gender: "male",
+// }
 
-console.log(whh);
+// console.log(whh);
 
 //工厂函数 factory function 
-function user(name, age, gender){
-    let person  = {};
-    person.name = name;
-    person.age = age;
-    person.gender = gender;
-    return person;
-}
+// function user(name, age, gender) {
+//     let person = {};
+//     person.name = name;
+//     person.age = age;
+//     person.gender = gender;
+//     return person;
+// }
 
-console.log(user("whh", 18, "male"));
+// console.log(user("whh", 18, "male"));
 
 // 构造函数
 // function User(name, age, gender){
@@ -187,47 +187,102 @@ console.log(user("whh", 18, "male"));
 //     this.gender = gender;
 // }
 
-class User {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    }
-}
+// class User {
+//     constructor(name, age, gender) {
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     }
+// }
 
 //实例化需要 new 
-console.log(new User("lsd", 10, "female"))
+// console.log(new User("lsd", 10, "female"))
 
-function Dog() {
-    this.age = 5;
-    this.name = "ww"
-    this.gender = "male";
-    this.act = function(){
-        console.log("act")
-    }
+// function Dog() {
+//     this.age = 5;
+//     this.name = "ww"
+//     this.gender = "male";
+//     this.act = function () {
+//         console.log("act")
+//     }
 
-    this.greeting = function () {
-        console.log("我叫" + this.name)
-    }
-}
+//     this.greeting = function () {
+//         console.log("我叫" + this.name)
+//     }
+// }
 
-console.log( new Dog );
+// console.log(new Dog);
 
 //原型 prototype 
-function Person(name, age, gender) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+// function Person(name, age, gender) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+// }
+
+// Person.prototype.greeting = function () {
+//     console.log("我叫" + this.name);
+// }
+
+// let a = new Person('王花花', 19, "male");
+
+// let b = new Person("赵柯爽", 20, "female");
+
+// console.log(a, b);
+// console.log(a.__proto__.greeting === b.__proto__.greeting);
+
+// console.log({});
+
+//原型的多级继承
+// 动物 > 哺乳动物 > 人类 > lsd
+
+// function Animal() { }
+
+// Animal.prototype.eat = function () {
+//     console.log("eat");
+// }
+
+// Animal.prototype.sleep = function () {
+//     console.log("sleep");
+// }
+
+// let ab = new Animal();
+// let bb = new Animal();
+
+// console.log(ab, bb)
+// console.log(ab.eat === bb.eat)
+
+// function Mammal() { }
+
+// //继承
+// Mammal.prototype = Object.create(Animal.prototype);
+// Mammal.prototype.constructor = Mammal;
+
+// Mammal.prototype.suckle = function () {
+//     console.log("mia mia")
+// };
+
+// function Person() { };
+
+// Person.prototype = Object.create(Mammal.prototype);
+// Person.prototype.constructor = Person;
+// Person.prototype.lie = function() {
+//     console.log("lie")
+// };
+// let p = new Person();
+
+// console.log(p)
+
+// let mm = new Mammal();
+// console.log(mm);
+
+function interval(fn) {
+    setTimeout(() => {
+        fn();
+        interval(fn);
+    }, 1000);
 }
 
-Person.prototype.greeting = function() {
-    console.log("我叫" + this.name);
-}
+let fn = function () {};
 
-let a = new Person('王花花', 19, "male");
-
-let b = new Person("赵柯爽", 20, "female");
-
-console.log(a,b);
-console.log(a.__proto__.greeting === b.__proto__.greeting)
-
+interval(fn);
